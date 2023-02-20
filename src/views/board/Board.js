61ff9159b247d450b70af06d8b14bc08339d9354
Reza Amraei <amraeireza@gmail.com>
2023-02-19 16:53:11 -0800

    feat: made all white pieces unique

--- src/views/board/Board.js
+++ src/views/board/Board.js
@@ -4,76 +4,87 @@ import "./board.css";
 
 const Board = () => {
   const [activePiece, setActivePiece] = useState(null);
+  //white
+  const pawnWhiteA2 = pawn("#dfdfdf");
+  const pawnWhiteB2 = pawn("#dfdfdf");
+  const pawnWhiteC2 = pawn("#dfdfdf");
+  const pawnWhiteD2 = pawn("#dfdfdf");
+  const pawnWhiteE2 = pawn("#dfdfdf");
+  const pawnWhiteF2 = pawn("#dfdfdf");
+  const pawnWhiteG2 = pawn("#dfdfdf");
+  const pawnWhiteH2 = pawn("#dfdfdf");
 
-  const pawnUser = pawn("#dfdfdf");
-  const rookUser = rook("#dfdfdf");
-  const bishopUser = bishop("#dfdfdf");
-  const knightUser = knight("#dfdfdf");
-  const queenUser = queen("#dfdfdf");
-  const kingUser = king("#dfdfdf");
+  const rookWhiteA1 = rook("#dfdfdf");
+  const knightWhiteB1 = knight("#dfdfdf");
+  const bishopWhiteC1 = bishop("#dfdfdf");
+  const queenWhiteD1 = queen("#dfdfdf");
+  const kingWhiteE1 = king("#dfdfdf");
+  const bishopWhiteF1 = bishop("#dfdfdf");
+  const knightWhiteG1 = knight("#dfdfdf");
+  const rookWhiteH1 = rook("#dfdfdf");
+  //black
+  const pawnBlackA7 = pawn("#565352");
+  const pawnBlackB7 = pawn("#565352");
+  const pawnBlackC7 = pawn("#565352");
+  const pawnBlackD7 = pawn("#565352");
+  const pawnBlackE7 = pawn("#565352");
+  const pawnBlackF7 = pawn("#565352");
+  const pawnBlackG7 = pawn("#565352");
+  const pawnBlackH7 = pawn("#565352");
 
-  const pawnOppA7 = pawn("#565352");
-  const pawnOppB7 = pawn("#565352");
-  const pawnOppC7 = pawn("#565352");
-  const pawnOppD7 = pawn("#565352");
-  const pawnOppE7 = pawn("#565352");
-  const pawnOppF7 = pawn("#565352");
-  const pawnOppG7 = pawn("#565352");
-  const pawnOppH7 = pawn("#565352");
-
-  const rookOppA8 = rook("#565352");
-  const rookOppH8 = rook("#565352");
-  const knightOppB8 = knight("#565352");
-  const knightOppG8 = knight("#565352");
-  const bishopOppC8 = bishop("#565352");
-  const bishopOppF8 = bishop("#565352");
-  const queenOpp = queen("#565352");
-  const kingOpp = king("#565352");
+  const rookBlackA8 = rook("#565352");
+  const rookBlackH8 = rook("#565352");
+  const knightBlackB8 = knight("#565352");
+  const knightBlackG8 = knight("#565352");
+  const bishopBlackC8 = bishop("#565352");
+  const bishopBlackF8 = bishop("#565352");
+  const queenBlack = queen("#565352");
+  const kingBlack = king("#565352");
 
   const [boardArr, setBoardArr] = useState([
     [
-      rookOppA8,
-      knightOppB8,
-      bishopOppC8,
-      queenOpp,
-      kingOpp,
-      bishopOppF8,
-      knightOppG8,
-      rookOppH8,
+      rookBlackA8,
+      knightBlackB8,
+      bishopBlackC8,
+      queenBlack,
+      kingBlack,
+      bishopBlackF8,
+      knightBlackG8,
+      rookBlackH8,
     ],
     [
-      pawnOppA7,
-      pawnOppB7,
-      pawnOppC7,
-      pawnOppD7,
-      pawnOppE7,
-      pawnOppF7,
-      pawnOppG7,
-      pawnOppH7,
+      pawnBlackA7,
+      pawnBlackB7,
+      pawnBlackC7,
+      pawnBlackD7,
+      pawnBlackE7,
+      pawnBlackF7,
+      pawnBlackG7,
+      pawnBlackH7,
     ],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [
-      pawnUser,
-      pawnUser,
-      pawnUser,
-      pawnUser,
-      pawnUser,
-      pawnUser,
-      pawnUser,
-      pawnUser,
+      pawnWhiteA2,
+      pawnWhiteB2,
+      pawnWhiteC2,
+      pawnWhiteD2,
+      pawnWhiteE2,
+      pawnWhiteF2,
+      pawnWhiteG2,
+      pawnWhiteH2,
     ],
     [
-      rookUser,
-      knightUser,
-      bishopUser,
-      queenUser,
-      kingUser,
-      bishopUser,
-      knightUser,
-      rookUser,
+      rookWhiteA1,
+      knightWhiteB1,
+      bishopWhiteC1,
+      queenWhiteD1,
+      kingWhiteE1,
+      bishopWhiteF1,
+      knightWhiteG1,
+      rookWhiteH1,
     ],
   ]);
   const removePossibleMovesFromBoard = () => {
